c49c98e042f6b415039b2b69bb7ada5b7281caa6
Jawad Khane <jawadkhan.cse@gmail.com>
2025-04-11 19:25:43 +0500

    fixed Bounded

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,20 +1,24 @@
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<C extends React.ElementType = "section"> = {
+  as?: C;
   className?: string;
   children: React.ReactNode;
-};
+} & React.ComponentPropsWithoutRef<C>;
 
-export const Bounded = ({ as: Comp = "section", className, children, ...restProps }: BoundedProps) => {
+
+export const Bounded = <C extends React.ElementType = "section">({
+  as: Comp = "section",
+  className,
+  children,
+  ...restProps
+}: BoundedProps<C>) => {
   return (
-    <Comp
-      className={clsx("px-4 first:pt-10 md:px-6", className)}
-      {...restProps}
-    >
+    <Comp className={clsx("px-4 first:pt-10 md:px-6", className)} {...restProps}>
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
     </Comp>
   );
 };
+
